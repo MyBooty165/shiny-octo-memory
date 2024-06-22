ca220e33d69b90f29afe99690b4bb8299c98bd65
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-06-22 18:29:39 -0500

    Update theme.js

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,14 +1,19 @@
 function theme() {
   const body = document.body;
   const logo = document.querySelector('.img-logo');
+  const themeicon = document.querySelector('.theme-changer i');
   const currentTheme = body.dataset.theme || 'light';
 
   if (currentTheme === 'light') {
     body.dataset.theme = 'dark';
     logo.src = '/static/images/inverted.png';
+    themeicon.classList.remove('fa-moon');
+    themeicon.classList.add('fa-sun');
   } else {
     body.dataset.theme = 'light';
     logo.src = '/static/images/icon.png';
+    themeicon.classList.remove('fa-sun');
+    themeicon.classList.add('fa-moon');
   }
 
   body.classList.toggle('dark-mode');
